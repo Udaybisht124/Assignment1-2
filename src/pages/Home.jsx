979613c268ba469f1a86c72637c46a2e779632e8
“uday <bishtuday4668@gmail.com>
2025-05-21 12:26:56 +0530

    updating the alignment of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,22 +33,22 @@ const Home = () => {
         marginBottom: 70,
       }}
     >
-      <div className="text-center max-w-2xl" style={{paddingTop:100}}>
-        <h1 className="text-5xl animate-pulse font-extrabold mb-6 mt-10 text-gray-800 dark:text-white leading-snug">
+      <div className="text-center max-w-2xl" style={{paddingTop:30}}>
+        <h1 className="text-5xl animate-pulse font-extrabold mb-6 mt-4 text-gray-800 dark:text-white leading-snug">
           Welcome to <span className="text-blue-500">Addval Solutions</span>
         </h1>
-        <p className="text-lg mb-8 my-10 animate-pulse font-bold text-gray-500 dark:text-black">
+        <p className="text-lg mb-8 my-12 animate-pulse font-bold text-gray-500 dark:text-black">
           Transform your dreams into reality with our innovative solutions! Let us help you achieve success with cutting-edge expertise.
         </p>
 
         <div
           className="bg-blue-700 dark:bg-blue-700 text-white dark:text-black w-48 hover:bg-blue-700 dark:hover:bg-blue-500 my-8 animate-none px-6 py-3 text-lg rounded-lg shadow-lg"
-          style={{ marginLeft: 210, marginTop: 80}}
+          style={{ marginLeft: 210, marginTop: 100}}
         >
           Get Started
         </div>
       </div>
-      <div className="flex justify-between p-4 gap-6 flex-wrap">
+      <div className="flex justify-between p-4 gap-6 flex-wrap" style={{marginTop:60}}>
         {cardData.map((card, index) => (
           <Component
             key={index}
