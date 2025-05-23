2066db2a47cd37ec57152a26b1fdc2bff3ecccdc
udaybisht124 <bishtuday4668@gmail.com>
2025-05-23 15:30:48 +0530

    adding the menu-items animation

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Navbar } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import { useThemeStore } from '../Store/ThemeStore'; // Import theme store
-import { useAuthStore } from '../Store/AuthStore'; // Import auth store
+import { useThemeStore } from '../Store/ThemeStore';
+import { useAuthStore } from '../Store/AuthStore';
 
 const AppNavbar = () => {
-  const { isLoggedIn, logout } = useAuthStore(); // Check if the user is logged in and provide a logout function
-  const { theme, toggleTheme } = useThemeStore(); // Access theme state and toggle function
+  const { isLoggedIn, logout } = useAuthStore();
+  const { theme, toggleTheme } = useThemeStore();
 
   return (
     <Navbar
       fluid={true}
       rounded={true}
       className="text-white h-20 dark:from-gray-800 dark:via-gray-700 dark:to-gray-600"
-      style={{backgroundColor:"#0d1013"}}
+      style={{ backgroundColor: "#0d1013" }}
     >
       <Navbar.Brand href="/">
         <img
@@ -21,7 +21,7 @@ const AppNavbar = () => {
           className="mr-3 h-6 sm:h-9 py-10 my-auto ml-4"
           alt="Addval Logo"
         />
-        <span className="self-center whitespace-nowrap  my-auto text-xl font-semibold dark:text-gray-200">
+        <span className="self-center whitespace-nowrap my-auto text-xl font-semibold dark:text-gray-200">
           Addval Solutions
         </span>
       </Navbar.Brand>
@@ -30,42 +30,34 @@ const AppNavbar = () => {
         {/* Navigation Links */}
         <Link
           to="/"
-          className="relative group text-white dark:text-gray-300 hover:text-yellow-300 hover:bg-green-400 transition duration-300 ease-in-out px-3"
+          className="nav-underline relative group text-white dark:text-gray-300 transition duration-300 ease-in-out px-3"
         >
           Home
-          <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-yellow-300 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </Link>
         <Link
           to="/services"
-          className="relative group text-white dark:text-gray-300 hover:text-yellow-300 transition duration-300 ease-in-out px-3"
+          className="nav-underline relative group text-white dark:text-gray-300 transition duration-300 ease-in-out px-3"
         >
           Services
-          <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-yellow-300 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </Link>
         <Link
           to="/contact"
-          className="relative group text-white dark:text-gray-300 hover:text-yellow-300 transition duration-300 ease-in-out px-3"
+          className="nav-underline relative group text-white dark:text-gray-300 transition duration-300 ease-in-out px-3"
         >
           Contact Us
-          <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-yellow-300 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </Link>
         <Link
           to="/clients"
-          className="relative group text-white dark:text-gray-300 hover:text-yellow-300 transition duration-300 ease-in-out px-3"
+          className="nav-underline relative group text-white dark:text-gray-300 transition duration-300 ease-in-out px-3"
         >
           Clients
-          <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-yellow-300 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </Link>
-
- <Link
+        <Link
           to="/ourwork"
-          className="relative group text-white dark:text-gray-300 hover:text-yellow-300 transition duration-300 ease-in-out px-3"
+          className="nav-underline relative group text-white dark:text-gray-300 transition duration-300 ease-in-out px-3"
         >
-        Our Work
-          <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-yellow-300 group-hover:w-full transition-all duration-300 ease-in-out"></span>
+          Our Work
         </Link>
-
-
 
         {/* Theme Toggle Button - Only Visible When Logged In */}
         {isLoggedIn && (
