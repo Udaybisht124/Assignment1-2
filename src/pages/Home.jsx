c2a1302aa8cd614ac681b67e3747f5d8c05d37ec
“uday <bishtuday4668@gmail.com>
2025-05-27 10:36:38 +0530

    updating the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       }}
     >
       <div className="flex flex-col items-center justify-center text-center max-w-2xl w-full">
-        <h1 className="text-4xl sm:text-5xl 2xl:text-9xl animate-pulse font-extrabold mb-6 text-gray-800 dark:text-black leading-snug"
+        <h1 className="text-4xl sm:text-5xl 2xl:text-[400px] animate-pulse font-extrabold mb-6 text-gray-800 dark:text-black leading-snug"
 
         style={{marginTop:-150}}
 >
@@ -30,7 +30,7 @@ const Home = () => {
           Transform your dreams into reality with our innovative solutions! Let us help you achieve success with cutting-edge expertise.
         </p>
         <div
-          className="bg-red-700 dark:bg-blue-700 text-white dark:text-black w-40 sm:w-48 hover:bg-blue-700 dark:hover:bg-blue-500 mb-8 px-6 py-3 text-lg rounded-lg shadow-lg mx-auto cursor-pointer transition"
+          className="bg-blue-500 hover:bg-blue-700 text-white dark:text-black w-40 sm:w-48 dark:bg-blue-700 hover:bg-blue-500 mb-8 px-6 py-3 text-lg rounded-lg shadow-lg mx-auto cursor-pointer transition"
         style={{marginTop:40}}
         >
           Get Started
