dc036f278b243370a7274d2408fab1a9e57fa592
udaybisht124 <bishtuday4668@gmail.com>
2025-06-26 18:38:25 +0530

    Make Component Responsive

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -108,21 +108,18 @@ export function ContactUs() {
   };
 
   return (
-    <div
-      className="flex flex-col lg:flex-row lg:gap-8 items-center justify-center h-screen max-w-full bg-gray-100 dark:bg-gray-900 px-4 py-4 lg:px-auto lg:py-10"
-      style={{ paddingBottom: 220 }}
-    >
-      <div className="flex-shrink-0 mb-10 lg:mb-32 lg:mr-16 flex justify-center items-center">
+    <div className="flex flex-col lg:flex-row lg:gap-8 items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 px-4" style={{paddingTop:"150px", paddingBottom:"150px"}}>
+      <div className="flex-shrink-0 mb-10 lg:mb-32 lg:mr-16 flex justify-center items-center ">
         <img
           src="https://addvalsolutions.com/assets/images/team/kulcare-team.webp"
           alt="Team"
-          className="h-auto mb-20 rounded-lg shadow-lg object-contain"
-          style={{ width: "450px", marginBottom: "100px", marginRight: "20px" }}
+          className="h-auto rounded-lg shadow-lg object-contain"
+          style={{ width: "450px" }}
         />
       </div>
-      <div className="flex flex-col w-full max-w-xl lg:max-w-full lg:mt-32 lg:h-36">
+      <div className="flex flex-col w-full max-w-xl lg:max-w-full lg:mt-32">
         <div className="py-10 mb-10 mt-10 text-5xl mx-auto font-bold text-blue-400 uppercase open-sans">
-          Contact US
+          Contact Us
         </div>
         {alert.show && (
           <Alert
@@ -132,58 +129,56 @@ export function ContactUs() {
             {alert.message}
           </Alert>
         )}
-        <div>
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col bg-white dark:bg-gray-800 dark:text-white w-full p-6 rounded-lg shadow-lg"
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col bg-white dark:bg-gray-800 dark:text-white w-full p-6 rounded-lg shadow-lg sm:pb-10 "
+        >
+          <div>
+            <div className="mb-2 block">
+              <Label
+                htmlFor="name"
+                className="text-gray-700 dark:text-gray-300"
+              >
+                Name
+              </Label>
+            </div>
+            <TextInput
+              id="name"
+              type="text"
+              placeholder="Enter your name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            />
+          </div>
+          <div className="mt-4">
+            <div className="mb-2 block">
+              <Label
+                htmlFor="message"
+                className="text-gray-700 dark:text-gray-300"
+              >
+                Message
+              </Label>
+            </div>
+            <Textarea
+              id="message"
+              placeholder="Type your message here..."
+              value={formData.message}
+              onChange={handleChange}
+              required
+              rows={4}
+              maxLength={100}
+              className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            />
+          </div>
+          <Button
+            type="submit"
+            className="mt-6 bg-blue-400 text-white hover:bg-blue-600 dark:bg-blue-500 dark:hover:bg-blue-700 transition-all duration-200"
           >
-            <div>
-              <div className="mb-2 block">
-                <Label
-                  htmlFor="name"
-                  className="text-gray-700 dark:text-gray-300"
-                >
-                  Name
-                </Label>
-              </div>
-              <TextInput
-                id="name"
-                type="text"
-                placeholder="Enter your name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-                className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              />
-            </div>
-            <div className="mt-4">
-              <div className="mb-2 block">
-                <Label
-                  htmlFor="message"
-                  className="text-gray-700 dark:text-gray-300"
-                >
-                  Message
-                </Label>
-              </div>
-              <Textarea
-                id="message"
-                placeholder="Type your message here..."
-                value={formData.message}
-                onChange={handleChange}
-                required
-                rows={4}
-                maxLength={100}
-                className="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              />
-            </div>
-            <Button
-              type="submit"
-              className="mt-6 bg-blue-400 text-white hover:bg-blue-600 dark:bg-blue-500 dark:hover:bg-blue-700 animate-pulse"
-            >
-              Submit
-            </Button>
-          </form>
-        </div>
+            Submit
+          </Button>
+        </form>
       </div>
     </div>
   );
